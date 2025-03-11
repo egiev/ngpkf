0c7015c0827564a10c37935b1e67c1c1422d0513
Reginald Mabanta <reginaldventura23@gmail.com>
2025-03-11 12:08:40 +0800

    add custom exception filter

---
+++ src/exceptions/entity-not-found.filter.ts
@@ -0,0 +1,11 @@
+import { ArgumentsHost, Catch, NotFoundException } from '@nestjs/common';
+import { GqlArgumentsHost, GqlExceptionFilter } from '@nestjs/graphql';
+import { NotFoundError } from '@mikro-orm/core';
+
+@Catch(NotFoundError)
+export class EntityNotFoundFilter<T> implements GqlExceptionFilter {
+  catch(exception: NotFoundError, host: ArgumentsHost) {
+    GqlArgumentsHost.create(host);
+    return new NotFoundException(exception.message);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EntityNotFoundFilter } from './exceptions/entity-not-found.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Global Exception Filter
+  app.useGlobalFilters(new EntityNotFoundFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
