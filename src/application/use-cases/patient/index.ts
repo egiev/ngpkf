46e6f07610e196813149bf344e87cc621be44f64
Reginald Mabanta <rvmabanta@themedicalcity.com>
2025-05-20 18:51:34 +0800

    remove unused files

--- src/application/use-cases/patient/index.ts
+++ src/application/use-cases/patient/index.ts
@@ -1,3 +1,1 @@
-export * from './create-patient.case';
 export * from './find-patient.case';
-export * from './findall-patient.case';
--- src/infrastructure/database/mongo/providers/patient.provider.ts
+++ src/infrastructure/database/mongo/providers/patient.provider.ts
@@ -1,21 +1,14 @@
 import { Provider } from '@nestjs/common';
-import { MessageBroker } from '@core/abstracts';
 import {
   AllergyDocumentsRepository,
   PatientRepository,
 } from '@core/repositories';
-import {
-  CreatePatientCase,
-  FindAllPatientCase,
-  FindPatientCase,
-} from '@application/use-cases/patient';
+import { FindPatientCase } from '@application/use-cases/patient';
 import { createUseCaseProvider } from '@shared/utils';
 import { AllergyDocumentsService, PatientService } from '../implementations';
 
 export const patientProvider: Provider[] = [
   { provide: PatientRepository, useClass: PatientService },
   { provide: AllergyDocumentsRepository, useClass: AllergyDocumentsService },
-  createUseCaseProvider(FindAllPatientCase, [PatientRepository]),
   createUseCaseProvider(FindPatientCase, [PatientRepository]),
-  createUseCaseProvider(CreatePatientCase, [PatientRepository, MessageBroker]),
 ];
