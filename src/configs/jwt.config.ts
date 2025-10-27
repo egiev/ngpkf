0507a766620d3ba1c96d9e19765ad9eaa45a92bd
Reginald Mabanta <reginaldventura23@gmail.com>
2025-10-27 14:31:00 +0800

    fix(docker): build for dev and prod

--- src/configs/jwt.config.ts
+++ src/configs/jwt.config.ts
@@ -1,9 +1,10 @@
 import { registerAs } from '@nestjs/config';
+import { StringValue } from 'ms';
 import { ENUM_CONFIG_KEY } from '@/configs/constants';
 
 export default registerAs(ENUM_CONFIG_KEY.JWT, () => ({
-  secret: process.env.JWT_SECRET,
+  secret: process.env.JWT_SECRET || 'secr3t-k3y',
   signOptions: {
-    expiresIn: process.env.JWT_EXPIRATION_TIME || '1h',
+    expiresIn: (process.env.JWT_EXPIRATION_TIME || '1d') as StringValue,
   },
 }));
--- src/configs/refresh-jwt.config.ts
+++ src/configs/refresh-jwt.config.ts
@@ -1,7 +1,8 @@
 import { registerAs } from '@nestjs/config';
+import { StringValue } from 'ms';
 import { ENUM_CONFIG_KEY } from '@/configs/constants';
 
 export default registerAs(ENUM_CONFIG_KEY.RefreshJWT, () => ({
-  secret: process.env.JWT_REFRESH_SECRET,
-  expiresIn: process.env.JWT_REFRESH_EXPIRATION_TIME,
+  secret: process.env.JWT_REFRESH_SECRET || 'secr3t-k3y',
+  expiresIn: (process.env.JWT_REFRESH_EXPIRATION_TIME || '1d') as StringValue,
 }));
