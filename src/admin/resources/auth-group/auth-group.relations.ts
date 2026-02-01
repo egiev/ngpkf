52b64e1a6bc2f40dfda3e1de2a39ffbb5ad8c2db
Reginald Mabanta <reginaldventura23@gmail.com>
2026-02-01 17:13:51 +0800

    fix: permission resource

--- src/admin/resources/auth-group/auth-group.relations.ts
+++ src/admin/resources/auth-group/auth-group.relations.ts
@@ -11,7 +11,7 @@ export function createGroupRelations(context: AdminContext): RelationsFeatureOpt
         throughResourceId: 'GroupPermissionEntity',
       },
       target: {
-        resourceId: 'PermissionEntity',
+        resourceId: 'AuthPermissionEntity',
       },
       deleteOptions: {
         enableDeleteRelation: false,
